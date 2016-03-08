c432d3656fa6d5b016bd90c26d38092fd1393174
jsohn96 <jsohn96@hotmail.com>
2016-03-08 00:25:58 -0800

    Position Syncing and Smoothing
    Removed Build
    
    Networking and Syncing

---
+++ Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour {
+
+	[SyncVar] private Quaternion syncPlayerRotation;
+	[SyncVar] private Quaternion syncCamRotation;
+
+	[SerializeField] private Transform playerTransform;
+	[SerializeField] private Transform camTransform;
+	[SerializeField] private float lerpRate = 15f;
+
+	private Quaternion lastPlayerRot;
+	private Quaternion lastCamRot;
+	private float threshold = 5f;
+
+	void Update () {
+		LerpRotations();
+	}
+
+	void FixedUpdate () {
+		TransmitRotations();
+	}
+
+	void LerpRotations(){
+		if(!isLocalPlayer){
+			playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation, Time.deltaTime*lerpRate);
+			camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, Time.deltaTime*lerpRate);
+		}
+	}
+
+	[Command]
+	void CmdProvideRotationsToServer(Quaternion playerRot, Quaternion camRot){
+		syncPlayerRotation = playerRot;
+		syncCamRotation = camRot;
+	}
+
+	[ClientCallback]
+	void TransmitRotations() {
+		if(isLocalPlayer) {
+			if(Quaternion.Angle(playerTransform.rotation, lastPlayerRot)>threshold || Quaternion.Angle(camTransform.rotation, lastCamRot)>threshold){
+				CmdProvideRotationsToServer(playerTransform.rotation, camTransform.rotation);	
+				lastPlayerRot = playerTransform.rotation;
+				lastCamRot = camTransform.rotation;
+			}
+		}
+	}
+}
